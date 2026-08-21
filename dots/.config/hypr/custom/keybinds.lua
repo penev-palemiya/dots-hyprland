91dbd3063e2d14ac15e04ff7820241b82f0cb280
Vladyslav Penev <penev.vladislav@gmail.com>
2026-08-21 13:38:21 +0300

    feat(Keybinds): add keybinding for switching to the previous workspace

--- dots/.config/hypr/custom/keybinds.lua
+++ dots/.config/hypr/custom/keybinds.lua
@@ -1,1 +1,3 @@
 hl.bind("CTRL+SUPER+ALT+Slash", hl.dsp.exec_cmd("xdg-open ~/.config/hypr/custom/keybinds.lua"), {description = "Edit user keybinds"} )
+
+hl.bind("ALT + Tab", hl.dsp.focus({ workspace = "previous" }), { description = "Workspace: Switch to previous" })
